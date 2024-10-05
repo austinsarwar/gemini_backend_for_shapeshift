3a86cc737ec7da7ad5a74379430547829406430e
austinsarwar <austin_sarwar@yahoo.com>
2024-10-05 18:11:49 -0400

    add open ai api key and history for basic chat

--- dynamic_chat.py
+++ dynamic_chat.py
@@ -7,7 +7,7 @@ from vertex_chat import generate_vertex_repsonse
 
 chat_history = []
 logger = logging.getLogger("uvicorn")
-def generate_dynamic_response(search_string):
+def  generate_dynamic_response(search_string):
     load_dotenv()
     # Configure the API key
     api_key = os.getenv("GEMINI_API_KEY")
@@ -25,18 +25,7 @@ def generate_dynamic_response(search_string):
     model_name="gemini-1.5-flash-002",
     generation_config=generation_config,
     system_instruction=[
-
-            """
-                You are Hercules, a highly advanced AI nutrition and fitness coach designed to help users gain muscle and weight. 
-                Your role is to generate personalized meal and workout plans that are tailored to each user's specific needs and fitness goals. 
-                You use cutting-edge AI models to dynamically adjust recommendations based on user inputs such as current stats, dietary preferences, 
-                 and progress over time.
-                Your primary focus is on creating high-calorie, protein-rich meal plans and strength-building exercise routines that promote muscle growth. 
-                Always ensure your guidance is actionable, clear, and tailored to individual preferences, while staying up-to-date with the latest nutrition and f
-                itness science.      
-            """
-
-
+                    "You are a helpful assistant"
         ],
     )
 
